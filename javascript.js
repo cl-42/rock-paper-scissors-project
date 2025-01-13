2a43498cb14d69373e26ae74a9959445f60d74b7
cl-42 <182110657+cl-42@users.noreply.github.com>
2025-01-13 18:29:45 +0800

    Complete algorithm to play single round
    Script now runs to play single round of rock, paper,
    scissors

--- javascript.js
+++ javascript.js
@@ -22,19 +22,53 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
-
 // Create function that logs user's choice
 function getHumanChoice() {
-    return prompt("Your move: Rock, Paper or Scissors?","")
+    return prompt("Your move: Rock, Paper or Scissors?","").toLowerCase()
 }
 
-console.log(getHumanChoice())
 
 // Game will keep track of the players score. 
 // Write variables to keep track of the players score.
 
 // Create two new variables named humanScore and computerScore in the global scope.
 // Initialize those variables with the value of 0.
+let humanScore = 0;
+let computerScore = 0;
+
+// Write the logic to play a single round
+
+function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
+    if (humanChoice === "rock" && computerChoice === "Scissors") {
+        return "You win! Rock beats Scissors."
+    } else if (humanChoice === "rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats Rock."
+    } else if (humanChoice === "rock" && computerChoice === "Rock") {
+        return "It's a draw! You both chose Rock."
+    } else if (humanChoice === "paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock."
+    } else if (humanChoice === "paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beats Paper."
+    } else if (humanChoice === "paper" && computerChoice === "Paper") {
+        return "It's a draw! You both chose Paper."
+    } else if (humanChoice === "scissors" && computerChoice === "Paper") {
+        return "You win! Scissors beats Paper."
+    } else if (humanChoice === "scissors" && computerChoice === "Rock") {
+        return "You lose! Rock beats Scissors."
+    } else if (humanChoice === "scissors" && computerChoice === "Scissors") {
+        return "It's a draw! You both chose Scissors."
+    }
+}
+
+console.log(playRound())
 
 
+
+
+// scissors > paper
+// paper > rock
+// rock > scissors
+
+// scissors = scissors
+// rock = rock
+// paper = paper
